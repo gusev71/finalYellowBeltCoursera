c2d95397c37461328def9d338351d922a82e80d5
Sergey <serge-gusev-007@yandex.ru>
2019-02-07 00:05:46 +0300

    add Comparison in node

--- node.h
+++ node.h
@@ -1,12 +1,30 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include "token.h"
+
+enum LogicalOperation{
+    And,
+    Or
+};
+enum Comparison{
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Equal,
+    NotEqual
+};
+class Node{
+     bool Evaluate(const Date& date, const std::string& event) = 0;
+};
 class EmptyNode{
 
 };
 
 class DateComparisonNode{
-
+    DateComparisonNode(const Comparison& cmp, const Date& date);
+    bool Evaluate(const Date& date, const std::string& event);
 };
 
 class EventComparisonNode{
