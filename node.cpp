8062179331291727c38829d4c8bf509b539b550d
Unknown <serge-gusev-007@yandex.ru>
2019-02-07 22:50:00 +0300

    few changes in node

--- node.cpp
+++ node.cpp
@@ -4,3 +4,31 @@ Node::Node()
 {
 
 }
+
+bool DateComparisonNode::Evaluate(const Date &date, const string &event)
+{
+    switch (_cmp) {
+    case Less:
+        return date < _data;
+    case LessOrEqual:
+        return (date < _data) || (date == _data);
+    case Greater:
+        return date > _data;
+    case GreaterOrEqual:
+        return (date > _data) || (date == _data);
+    case Equal:
+        return date == _data;
+    case NotEqual:
+        return date != _data;
+    }
+}
+
+bool EventComparisonNode::Evaluate(const Date &date, const string &event)
+{
+    if(event == "ignore_event_") return true;
+}
+
+bool LogicalOperationNode::Evaluate(const Date &date, const string &event)
+{
+
+}
--- node.h
+++ node.h
@@ -2,11 +2,14 @@
 #define NODE_H
 
 #include "token.h"
+#include <date.h>
+#include <memory>
 
 enum LogicalOperation{
     And,
     Or
 };
+
 enum Comparison{
     Less,
     LessOrEqual,
@@ -15,29 +18,48 @@ enum Comparison{
     Equal,
     NotEqual
 };
-class Node{
-     bool Evaluate(const Date& date, const std::string& event) = 0;
-};
-class EmptyNode{
 
+class Node{
+public:
+    virtual bool Evaluate(const Date& date, const std::string& event) = 0;
+    virtual ~Node(){}
+};
+
+class EmptyNode{
+public:
+    bool Evaluate(const Date& date, const std::string& event) {return  true;}
 };
 
 class DateComparisonNode{
+public:
     DateComparisonNode(const Comparison& cmp, const Date& date);
-    bool Evaluate(const Date& date, const std::string& event);
+    virtual bool Evaluate(const Date& date, const std::string& event)override;
+
+private:
+    Comparison _cmp;
+    Date _data;
 };
 
 class EventComparisonNode{
+public:
+    EventComparisonNode(const Date& date, const std::string& event);
+    virtual bool Evaluate(const Date& date, const std::string& event)override;
 
+private:
+    Comparison _cmp;
+    std::string _event;
 };
 class LogicalOperationNode{
-
-};
-//----------------------------------
-class Node
-{
 public:
-    Node();
+    LogicalOperationNode(const LogicalOperation& lop,
+                         const shared_ptr<Node>& nodeLeft,
+                         const shared_ptr<Node>& nodeRight);
+    virtual bool Evaluate(const Date& date, const std::string& event)override;
+
+private:
+    LogicalOperation _lop;
+    shared_ptr<Node> _nodeLeft;
+    shared_ptr<Node> _nodeRight;
 };
 
 #endif // NODE_H
